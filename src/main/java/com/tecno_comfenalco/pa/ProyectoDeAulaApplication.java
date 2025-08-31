4820f1e1be0cb55fee26e9bb572d8038bedf9385
DJAYI <danilo.arenasyi@gmail.com>
2025-08-31 11:47:03 -0500

    Initial commit

---
+++ src/main/java/com/tecno_comfenalco/pa/ProyectoDeAulaApplication.java
@@ -0,0 +1,13 @@
+package com.tecno_comfenalco.pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDeAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDeAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecno_comfenalco/pa/ProyectoDeAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecno_comfenalco.pa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDeAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
