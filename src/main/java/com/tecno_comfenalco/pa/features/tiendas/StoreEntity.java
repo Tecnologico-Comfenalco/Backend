fe73ff94856c1ae7a5a002c0ffd72abe4718dc33
br7forlife <diazpachecobrayan@gmail.com>
2025-09-15 08:20:56 -0500

    create relation many to many with PresalesRoutesEntity

--- src/main/java/com/tecno_comfenalco/pa/features/tiendas/StoreEntity.java
+++ src/main/java/com/tecno_comfenalco/pa/features/tiendas/StoreEntity.java
@@ -1,14 +1,32 @@
 package com.tecno_comfenalco.pa.features.tiendas;
 
+import java.util.Set;
+
+import com.tecno_comfenalco.pa.features.rutas.proveedores.PresalesRoutesEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "stores")
+@Data
 public class StoreEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long NIT;
+
+    private String name;
+    private String direccion;
+    private String city;
+    private Long cellphone;
+    private String email;
+
+    @ManyToMany(mappedBy = "storeEntities")
+    private Set<PresalesRoutesEntity> presalesRoutesEntities;
 }
