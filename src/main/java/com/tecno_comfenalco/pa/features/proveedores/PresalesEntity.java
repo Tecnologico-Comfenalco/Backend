c738ffd46ade242787bd5d6a57c81e190d4784d7
br7forlife <diazpachecobrayan@gmail.com>
2025-09-15 08:20:14 -0500

    create relation one to many with PresalesRoutesentity

--- src/main/java/com/tecno_comfenalco/pa/features/proveedores/PresalesEntity.java
+++ src/main/java/com/tecno_comfenalco/pa/features/proveedores/PresalesEntity.java
@@ -1,14 +1,32 @@
 package com.tecno_comfenalco.pa.features.proveedores;
 
+import java.util.List;
+
+import com.tecno_comfenalco.pa.features.rutas.proveedores.PresalesRoutesEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "presales")
+@Data
 public class PresalesEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    private String document_type;
+    private Long document_number;
+    private Long cellphone;
+    private String email;
+    
+    @OneToMany(mappedBy = "presalesEntityMany")
+    private List<PresalesRoutesEntity> presellerRoutesEntityMany;
 }
