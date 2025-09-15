529fdffa62b471f63387860bf6fb1e12b3bd7d66
br7forlife <diazpachecobrayan@gmail.com>
2025-09-15 08:17:45 -0500

    add name in the table and create relation with ProductEntity

--- src/main/java/com/tecno_comfenalco/pa/features/catalogos/CatalogEntity.java
+++ src/main/java/com/tecno_comfenalco/pa/features/catalogos/CatalogEntity.java
@@ -1,14 +1,24 @@
 package com.tecno_comfenalco.pa.features.catalogos;
 
+import java.util.List;
+
+import com.tecno_comfenalco.pa.features.productos.ProductEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "catalogs")
 public class CatalogEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(mappedBy = "catalogEntity")
+    private List<ProductEntity> productEntities;
 }
