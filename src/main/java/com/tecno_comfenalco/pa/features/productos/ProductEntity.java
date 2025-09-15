e9df1378adc1936b19371f824430052462de21a8
br7forlife <diazpachecobrayan@gmail.com>
2025-09-15 08:19:19 -0500

    agg name in the tabla products, create some atributies, and finished with relations CatalogEntity

--- src/main/java/com/tecno_comfenalco/pa/features/productos/ProductEntity.java
+++ src/main/java/com/tecno_comfenalco/pa/features/productos/ProductEntity.java
@@ -1,14 +1,30 @@
 package com.tecno_comfenalco.pa.features.productos;
 
+import com.tecno_comfenalco.pa.features.catalogos.CatalogEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "products")
+@Data
 public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    private Float price;
+    private Integer unit;
+
+    @ManyToOne
+    @JoinColumn(name = "catalog_id")
+    private CatalogEntity catalogEntity;
 }
