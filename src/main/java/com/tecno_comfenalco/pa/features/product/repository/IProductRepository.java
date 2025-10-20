5be356c9ff744e6300518a392d9c11f4185de6bc
br7forlife <diazpachecobrayan@gmail.com>
2025-10-20 12:07:11 -0500

    defined repository methods

--- src/main/java/com/tecno_comfenalco/pa/features/product/repository/IProductRepository.java
+++ src/main/java/com/tecno_comfenalco/pa/features/product/repository/IProductRepository.java
@@ -1,5 +1,7 @@
 package com.tecno_comfenalco.pa.features.product.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.repository.NoRepositoryBean;
@@ -9,5 +11,17 @@ import com.tecno_comfenalco.pa.features.product.ProductEntity;
 
 @NoRepositoryBean
 public interface IProductRepository extends Repository<ProductEntity, UUID> {
+    ProductEntity save(ProductEntity productEntity);
 
+    Optional<ProductEntity> findById(UUID id);
+
+    Optional<ProductEntity> findByName(String name);
+
+    List<ProductEntity> findAll();
+
+    List<ProductEntity> findByCatalogId(Long id);
+
+    boolean existsByName(String name);
+
+    void deleteById(UUID id);
 }
