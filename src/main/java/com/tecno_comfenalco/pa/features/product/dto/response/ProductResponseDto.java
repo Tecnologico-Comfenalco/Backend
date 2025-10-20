be68ffd24be8fef4eeedcbc869ab78913016ed3b
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-10-20 18:09:14 -0500

    fix: update ProductResponseDto to use 'product' instead of 'productEntity'; correct database connection details in application-postgres.yml

--- src/main/java/com/tecno_comfenalco/pa/features/product/dto/response/ProductResponseDto.java
+++ src/main/java/com/tecno_comfenalco/pa/features/product/dto/response/ProductResponseDto.java
@@ -2,6 +2,6 @@ package com.tecno_comfenalco.pa.features.product.dto.response;
 
 import com.tecno_comfenalco.pa.features.product.ProductEntity;
 
-public record ProductResponseDto(String message,ProductEntity productEntity) {
-    
+public record ProductResponseDto(String message, ProductEntity product) {
+
 }
