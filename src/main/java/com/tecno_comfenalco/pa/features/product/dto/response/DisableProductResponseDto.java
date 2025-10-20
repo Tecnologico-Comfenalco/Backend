d4b631e86f761751462043ae1272eefb901ccf1c
br7forlife <diazpachecobrayan@gmail.com>
2025-10-20 12:08:08 -0500

    create dto response for product entity

---
+++ src/main/java/com/tecno_comfenalco/pa/features/product/dto/response/DisableProductResponseDto.java
@@ -0,0 +1,5 @@
+package com.tecno_comfenalco.pa.features.product.dto.response;
+
+public record DisableProductResponseDto(String message) {
+    
+}
---
+++ src/main/java/com/tecno_comfenalco/pa/features/product/dto/response/EditProductResponseDto.java
@@ -0,0 +1,7 @@
+package com.tecno_comfenalco.pa.features.product.dto.response;
+
+import java.util.UUID;
+
+public record EditProductResponseDto(String message, UUID id) {
+    
+}
