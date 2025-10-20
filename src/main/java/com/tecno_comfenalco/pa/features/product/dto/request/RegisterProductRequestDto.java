1f514409dd32256ab7c3f0dd75d9bcc0d645eec9
br7forlife <diazpachecobrayan@gmail.com>
2025-10-20 12:07:40 -0500

    create dto request for product entity

---
+++ src/main/java/com/tecno_comfenalco/pa/features/product/dto/request/RegisterProductRequestDto.java
@@ -0,0 +1,15 @@
+package com.tecno_comfenalco.pa.features.product.dto.request;
+
+import com.tecno_comfenalco.pa.features.product.ProductEntity.Unit;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record RegisterProductRequestDto(
+        @NotBlank(message = "El nombre no puede estar vacio o nulo!") String name,
+        @NotNull(message = "EL category_id no puede ser nulo!") @Positive(message = "El category_id no puede ser negativo!") Integer category_id,
+        @NotNull(message = "El precio del producto no puede ser nulo!") @Positive(message = "El precio del producto no puede ser negativo") Double price,
+        Unit unit) {
+
+}
