1ad749e83b2759462d571ad99bcda9f745df1753
br7forlife <diazpachecobrayan@gmail.com>
2025-10-22 12:38:54 -0500

    delivery repository

--- src/main/java/com/tecno_comfenalco/pa/features/delivery/repository/IDeliveryRepository.java
+++ src/main/java/com/tecno_comfenalco/pa/features/delivery/repository/IDeliveryRepository.java
@@ -1,5 +1,8 @@
 package com.tecno_comfenalco.pa.features.delivery.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
@@ -8,4 +11,13 @@ import com.tecno_comfenalco.pa.features.delivery.DeliveryEntity;
 @NoRepositoryBean
 public interface IDeliveryRepository extends Repository<DeliveryEntity, Long> {
 
+    Optional<DeliveryEntity> findById(Long id);
+
+    Optional<DeliveryEntity> findByName(String name);
+
+    List<DeliveryEntity> findAll();
+
+    DeliveryEntity save(DeliveryEntity presalesEntity);
+
+    boolean existsByDocumentNumber(Long documentNumber);
 }
