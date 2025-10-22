5d19a7c2d2d017f4e231a7be4a7d411b8cecc11d
br7forlife <diazpachecobrayan@gmail.com>
2025-10-22 10:57:59 -0500

    presales dto request

---
+++ src/main/java/com/tecno_comfenalco/pa/features/presales/dto/request/RegisterPresalesRequestDto.java
@@ -0,0 +1,15 @@
+package com.tecno_comfenalco.pa.features.presales.dto.request;
+
+import com.tecno_comfenalco.pa.shared.enums.DocumentTypeEnum;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+public record RegisterPresalesRequestDto(
+        @NotBlank(message = "El nombre del preventista no debe ser nulo ni vacio!") String name,
+        @NotBlank(message = "El numero de telefono no puede ser nulo ni vacio!") String phoneNumber,
+        @Email(message = "El email debe tener un formato correcto!") String email, DocumentTypeEnum documentTypeEnum,
+        @Positive(message = "El numero de documento de identidad debe ser positivo!") @NotBlank(message = "El numero de documento de identidad no debe ser nulo ni vacio!") Long documentNumber) {
+
+}
