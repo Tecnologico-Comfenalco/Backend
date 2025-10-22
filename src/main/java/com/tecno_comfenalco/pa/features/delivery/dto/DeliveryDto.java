2bb9b015331c6c91889f76e23a8bf67e232cc02a
br7forlife <diazpachecobrayan@gmail.com>
2025-10-22 12:38:37 -0500

    pincipal dto deliveryDto

--- src/main/java/com/tecno_comfenalco/pa/features/delivery/dto/DeliveryDto.java
+++ src/main/java/com/tecno_comfenalco/pa/features/delivery/dto/DeliveryDto.java
@@ -1,5 +1,9 @@
 package com.tecno_comfenalco.pa.features.delivery.dto;
 
-public record DeliveryDto() {
+import com.tecno_comfenalco.pa.features.delivery.DeliveryEntity.LicenseTypeEnum;
+import com.tecno_comfenalco.pa.shared.enums.DocumentTypeEnum;
+
+public record DeliveryDto(Long id, String name, DocumentTypeEnum documentType, Long documentNumber, String phoneNumber,
+        String licenseNumber, LicenseTypeEnum licenseType) {
 
 }
