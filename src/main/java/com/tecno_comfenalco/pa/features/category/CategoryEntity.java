e46ec150e686567c77ce3d8e5dea48b4527dade1
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-10-22 16:43:40 -0500

    feat: add CategoryEntity class with relationships to CatalogEntity and ProductEntity

---
+++ src/main/java/com/tecno_comfenalco/pa/features/category/CategoryEntity.java
@@ -0,0 +1,36 @@
+package com.tecno_comfenalco.pa.features.category;
+
+import java.util.List;
+
+import com.tecno_comfenalco.pa.features.catalog.CatalogEntity;
+import com.tecno_comfenalco.pa.features.product.ProductEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "categories_products", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<ProductEntity> products;
+
+    @ManyToOne
+    @JoinColumn(name = "catalog_id", nullable = false)
+    private CatalogEntity catalog;
+}
