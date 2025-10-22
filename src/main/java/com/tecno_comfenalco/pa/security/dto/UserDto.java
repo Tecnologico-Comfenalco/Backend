a3325bb62da58444f48ac4fce10668203715d9bb
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-10-22 16:01:33 -0500

    refactor: add user ID to UserDto for improved user identification

--- src/main/java/com/tecno_comfenalco/pa/security/dto/UserDto.java
+++ src/main/java/com/tecno_comfenalco/pa/security/dto/UserDto.java
@@ -2,6 +2,6 @@ package com.tecno_comfenalco.pa.security.dto;
 
 import java.util.Set;
 
-public record UserDto(String username, Set<String> roles, boolean enabled) {
+public record UserDto(Long id, String username, Set<String> roles, boolean enabled) {
 
 }
