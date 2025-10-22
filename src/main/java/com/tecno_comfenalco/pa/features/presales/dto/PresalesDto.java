784753c97ff5527ef11b6ceabcb06ba225aa0446
br7forlife <diazpachecobrayan@gmail.com>
2025-10-22 10:54:43 -0500

    add parameters in presales dto

--- src/main/java/com/tecno_comfenalco/pa/features/presales/dto/PresalesDto.java
+++ src/main/java/com/tecno_comfenalco/pa/features/presales/dto/PresalesDto.java
@@ -1,5 +1,8 @@
 package com.tecno_comfenalco.pa.features.presales.dto;
 
-public record PresalesDto() {
+import com.tecno_comfenalco.pa.shared.enums.DocumentTypeEnum;
+
+public record PresalesDto(Long id, String name, String phoneNumber, String email, DocumentTypeEnum documentTypeEnum,
+        Long documentNumber) {
 
 }
