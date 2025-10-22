5f838ec0b941cffc0299f00ec84bdae0f3128ab9
br7forlife <diazpachecobrayan@gmail.com>
2025-10-22 12:40:11 -0500

    delivery dto response and request

---
+++ src/main/java/com/tecno_comfenalco/pa/features/delivery/dto/response/DeliveryResponseDto.java
@@ -0,0 +1,7 @@
+package com.tecno_comfenalco.pa.features.delivery.dto.response;
+
+import com.tecno_comfenalco.pa.features.delivery.dto.DeliveryDto;
+
+public record DeliveryResponseDto(DeliveryDto deliveryDto, String message) {
+    
+}
