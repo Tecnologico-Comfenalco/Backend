91da59ca7ce1fa5345f2a3fb852f009bab603410
br7forlife <diazpachecobrayan@gmail.com>
2025-10-25 20:00:46 -0500

    add @Data lombok

--- src/main/java/com/tecno_comfenalco/pa/features/store/StoreEntity.java
+++ src/main/java/com/tecno_comfenalco/pa/features/store/StoreEntity.java
@@ -15,9 +15,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "stores")
+@Data
 public class StoreEntity {
 
     @Id
