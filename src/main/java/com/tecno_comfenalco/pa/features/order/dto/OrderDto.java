6da8c9e69efe32834796fa97501dff703b7a6b96
br7forlife <diazpachecobrayan@gmail.com>
2025-10-26 14:51:02 -0500

    create OrderDto

--- src/main/java/com/tecno_comfenalco/pa/features/order/dto/OrderDto.java
+++ src/main/java/com/tecno_comfenalco/pa/features/order/dto/OrderDto.java
@@ -1,5 +1,13 @@
 package com.tecno_comfenalco.pa.features.order.dto;
 
-public record OrderDto() {
+import java.util.List;
+import java.util.UUID;
+
+import com.tecno_comfenalco.pa.features.order.OrderDetailEntity;
+import com.tecno_comfenalco.pa.features.presales.PresalesEntity;
+import com.tecno_comfenalco.pa.features.store.StoreEntity;
+
+public record OrderDto(UUID id, Double iva_percent, Double total, StoreEntity store, PresalesEntity presales,
+        List<OrderDetailEntity> orderDetails) {
 
 }
