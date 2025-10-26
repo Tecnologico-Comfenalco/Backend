206aab929c3717414ed8514395ade7cc41abb65c
br7forlife <diazpachecobrayan@gmail.com>
2025-10-25 20:05:01 -0500

    create request store dto

---
+++ src/main/java/com/tecno_comfenalco/pa/features/store/dto/request/EditStoreRequestDto.java
@@ -0,0 +1,16 @@
+package com.tecno_comfenalco.pa.features.store.dto.request;
+
+import com.tecno_comfenalco.pa.shared.dto.DirectionDto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+public record EditStoreRequestDto(
+        @NotBlank(message = "El NIT de la tienda no puede ser nulo ni vacio!") @Positive(message = "El NIT de la tienda debe ser positivo!") Long NIT,
+        @NotBlank(message = "El nombre de la tienda no puede ser nulo ni vacio!") String name,
+        @NotBlank(message = "El telefono no debe ser nulo ni vacio!") String phoneNumber,
+        @Email(message = "El correo debe tener un formato correcto!") @NotBlank(message = "El email no debe ser nulo ni vacio!") String email,
+        DirectionDto direction) {
+
+}
