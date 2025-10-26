2a4ab51189772bc0c47e1855d0cfc7c20673ebfb
br7forlife <diazpachecobrayan@gmail.com>
2025-10-25 20:03:51 -0500

    finished vehicle repository

--- src/main/java/com/tecno_comfenalco/pa/features/vehicle/repository/IVehicleRepository.java
+++ src/main/java/com/tecno_comfenalco/pa/features/vehicle/repository/IVehicleRepository.java
@@ -1,5 +1,8 @@
 package com.tecno_comfenalco.pa.features.vehicle.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
@@ -7,5 +10,15 @@ import com.tecno_comfenalco.pa.features.vehicle.VehicleEntity;
 
 @NoRepositoryBean
 public interface IVehicleRepository extends Repository<VehicleEntity, Long> {
+    VehicleEntity save(VehicleEntity vehicleEntity);
 
+    Optional<VehicleEntity> findById(Long id);
+
+    Optional<VehicleEntity> findByVehiclePlate(String name);
+
+    List<VehicleEntity> findAll();
+
+    boolean existsByName(String name);
+
+    void deleteById(Long id);
 }
