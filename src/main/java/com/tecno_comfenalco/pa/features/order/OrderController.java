89b7dd09ad66a3a35036a771576cece7ea1d8ee8
DJAYI <danilo.arenasyi@gmail.com>
2025-10-27 00:11:08 -0500

    Refactor CreateOrderRequestDto validation annotations for clarity and consistency

--- src/main/java/com/tecno_comfenalco/pa/features/order/OrderController.java
+++ src/main/java/com/tecno_comfenalco/pa/features/order/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,6 @@ import com.tecno_comfenalco.pa.features.order.service.OrderService;
 import com.tecno_comfenalco.pa.shared.utils.helper.ResponseEntityHelper;
 import com.tecno_comfenalco.pa.shared.utils.result.Result;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
--- src/main/java/com/tecno_comfenalco/pa/features/order/dto/request/CreateOrderRequestDto.java
+++ src/main/java/com/tecno_comfenalco/pa/features/order/dto/request/CreateOrderRequestDto.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import com.tecno_comfenalco.pa.features.order.dto.OrderProductDto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CreateOrderRequestDto(
-        @NotBlank(message = "La id de la tienda no debe ser nulo ni vacio!") @Positive(message = "La id de la tienda debe ser un numero positivo!") Long store_id,
-        @NotBlank(message = "La id del preventista no debe ser nulo ni vacio!") @Positive(message = "La id del preventista debe ser positivo!") Long presales_id,
-        @NotNull(message = "La lista de productos no debe ser nulo!") List<OrderProductDto> productEntities) {
+                @NotNull(message = "La id de la tienda no debe ser nulo!") @Positive(message = "La id de la tienda debe ser un numero positivo!") Long store_id,
+                @NotNull(message = "La id del preventista no debe ser nulo!") @Positive(message = "La id del preventista debe ser positivo!") Long presales_id,
+                @NotNull(message = "La lista de productos no debe ser nulo!") List<OrderProductDto> productEntities) {
 
 }
